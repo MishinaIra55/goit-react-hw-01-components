878d11a301385be57a6ca09f1ca4db1c957db4b1
MishynaIra <gortenzia1987@gmail.com>
2022-12-15 12:59:34 +0200

    profil выносим в отдельный фаил и спользуем app

--- src/index.jsx
+++ src/index.jsx
@@ -2,50 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import user from './user.json/user.json';
 
 
 
 
-function Profile (props) {
-  const {avatar,username,tag,  location, followers, views, likes } = props;
-  return <div className="profile">
-    <div className="description">
-      <img
-        src={avatar}
-        alt="User avatar"
-        className="avatar"
-      />
-      <p className="name">{username}</p>
-      <p className="tag">{tag}</p>
-      <p className="location">{location}</p>
-    </div>
 
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{followers}</span>
-      </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity">{views}</span>
-      </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{likes}</span>
-      </li>
-    </ul>
-  </div>
-}
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Profile
-    avatar={user.avatar}
-    name={user.username}
-    tag={user.tag}
-    location={user.location}
-    followers={user.stats.followers}
-    views={user.stats.views}
-    likes={user.stats.likes}
-  />);
+  <App/>
+
+);
