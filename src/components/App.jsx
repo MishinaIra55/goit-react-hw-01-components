93e1efa09f38653516f81e22fe539a72733b932a
MishynaIra <gortenzia1987@gmail.com>
2022-12-19 14:37:39 +0200

    statistics add random color

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import Profile from 'components/Profile/Profile';
 import user from 'assets/user.json';
 
+import Statistics from './Statistics/Statistics';
+import data from 'assets/data.json';
 export const App = () => {
   return (
     <div
@@ -13,6 +15,7 @@ export const App = () => {
         color: '#010101',
         padding:50,
 
+
       }}
     >
       <Profile
@@ -23,6 +26,11 @@ export const App = () => {
         followers={user.stats.followers}
         views={user.stats.views}
         likes={user.stats.likes}
+      />
+
+      <Statistics
+        title="Upload stats"
+        stats={data}
       />
     </div>
   );
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,47 @@
+
+import styles from './Statistics.module.css';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+export default function Statistics (props) {
+  const { title, stats } = props;
+console.log(stats);
+    return (
+      <section className={styles.statistics}>
+        <h2 className={styles.title}>{title}</h2>
+
+        <ul className={styles.list}>
+          {
+            stats.map((stat) => {
+              return (
+                <li className={styles.item} key={stat.id}
+                    style={{
+                      backgroundColor: getRandomHexColor(),
+                    }}>
+                  <span className={styles.label}>{stat.label}</span>
+                  <span className={styles.percentage}>{stat.percentage + '%'}</span>
+
+
+                </li>
+              )
+            })
+          }
+
+
+        </ul>
+      </section>
+    )
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array
+}
+
+
